54dbc0c4161893b68c90660f99041cfb441fa479
CBolduc10 <44456874+CBolduc10@users.noreply.github.com>
2021-03-06 23:46:03 -0600

    Update test.java

--- GroupProject1_CBZE/src/test.java
+++ GroupProject1_CBZE/src/test.java
@@ -2,9 +2,5 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		System.out.println("Subscribe to my Twitch Channel ya heard me.");
-		System.out.println("Test1");
-		System.out.println("Ethan was here!");
-		System.out.println("Zach wuz here");
 	}
 }
