0043463d2d30f57016972893be1dc7610f35ef6a
CBolduc10 <44456874+CBolduc10@users.noreply.github.com>
2021-03-28 15:59:50 -0500

    Added more member data, now up to 5.

--- GroupProject1_CBZE/src/business/tests/AutomatedTester.java
+++ GroupProject1_CBZE/src/business/tests/AutomatedTester.java
@@ -11,12 +11,12 @@ import business.facade.Result;
 
 public class AutomatedTester {
 	private Store store;
-	private String[] memberNames = { "n1", "n2", "n3" };
-	private String[] memberAddresses = { "a1", "a2", "a3" };
-	private String[] memberPhones = { "p1", "p2", "p3" };
-	private String[] memberFeePaid = { "y", "n", "y"};
-	private String[] memberDateJoined = { "03/28/2021", "02/15/2021", "03/01/2021" };
-	private Member[] members = new Member[3];
+	private String[] memberNames = { "Colin", "Ethan", "Zach","Brian", "Brahma" };
+	private String[] memberAddresses = { "2641", "1165", "7798", "0445", "4653" };
+	private String[] memberPhones = { "651500", "789552", "744158", "466573", "892736" };
+	private String[] memberFeePaid = { "y", "n", "y", "n", "y"};
+	private String[] memberDateJoined = { "03/28/2021", "02/15/2021", "03/01/2021", "03/04/2021", "01/15/2021" };
+	private Member[] members = new Member[5];
 	private String[] productName = { "t1", "t2", "t3", "t4", "t5", "t6" };
 	private String[] productStock = { "1", "3", "5", "7", "9", "11" };
 	private String[] productPrice = { "2.50", "5.00", "7.50", "10.00", "12.50", "15.00" };
