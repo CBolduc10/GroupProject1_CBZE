b35b52bd840563d6da03a5f8080a9cac14a2370f
CBolduc10 <44456874+CBolduc10@users.noreply.github.com>
2021-03-28 20:02:21 -0500

    added another product

--- GroupProject1_CBZE/src/business/tests/AutomatedTester.java
+++ GroupProject1_CBZE/src/business/tests/AutomatedTester.java
@@ -17,12 +17,12 @@ public class AutomatedTester {
 	private String[] memberFeePaid = { "y", "y", "y", "y", "y", "n" };
 	private String[] memberDateJoined = { "03/28/2021", "02/15/2021", "03/01/2021", "03/04/2021", "01/15/2021", "12/29/2020" };
 	private Member[] members = new Member[6];
-	private String[] productName = { "prod1", "prod2", "prod3", "prod4", "prod5", "prod6", "prod7", "prod8", "prod9", "prod10", "prod11", "prod12", "prod13", "prod14", "prod15", "prod16", "prod17", "prod18", "prod19", "prod20" };
-	private String[] productStock = { "1", "3", "5", "7", "9", "11", "12", "15", "8", "9", "25", "16", "14", "6", "21", "30", "14", "34", "20", "10" };
-	private String[] productPrice = { "2.50", "5.00", "7.50", "10.00", "12.50", "15.00", "17.50", "20.00", "22.50", "25.00", "27.50", "30.00", "32.50", "35.00", "37.50", "40.00", "42.50", "45.00", "47.50", "50.00" };
-	private String[] productReorderLevel = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
-	private String[] productId = { "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "P11", "P12", "P13", "P14", "P15", "P16", "P17", "P18", "P19", "P20" };
-	private Product[] products = new Product[20];
+	private String[] productName = { "prod1", "prod2", "prod3", "prod4", "prod5", "prod6", "prod7", "prod8", "prod9", "prod10", "prod11", "prod12", "prod13", "prod14", "prod15", "prod16", "prod17", "prod18", "prod19", "prod20", "prod21" };
+	private String[] productStock = { "1", "3", "5", "7", "9", "11", "12", "15", "8", "9", "25", "16", "14", "6", "21", "30", "14", "34", "20", "10", "1" };
+	private String[] productPrice = { "2.50", "5.00", "7.50", "10.00", "12.50", "15.00", "17.50", "20.00", "22.50", "25.00", "27.50", "30.00", "32.50", "35.00", "37.50", "40.00", "42.50", "45.00", "47.50", "50.00", "55.00" };
+	private String[] productReorderLevel = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "5" };
+	private String[] productId = { "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "P11", "P12", "P13", "P14", "P15", "P16", "P17", "P18", "P19", "P20", "P21" };
+	private Product[] products = new Product[21];
 	
 	/**
 	 * Tests Member Creation
